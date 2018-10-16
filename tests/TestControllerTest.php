9ed996f49dcca78fd9f6073127396cea594a6291
Florian Chaudret <florian.chaudret@gmail.com>
2018-10-16 22:32:38 +0200

    Add first test unitaire

---
+++ tests/TestControllerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Tests;
+
+use App\Controller\TestController;
+
+/**
+ * Class TestControllerTest
+ */
+class TestControllerTest extends \PHPUnit\Framework\TestCase
+{
+    public const valueTest = 'statan';
+
+    public function testA(): void
+    {
+        $c = new TestController(self::valueTest);
+        $r = json_decode($c->test()->getContent(), true);
+        self::assertEquals('b', $r['a']);
+        self::assertEquals(self::valueTest, $r['test']);
+    }
+}
